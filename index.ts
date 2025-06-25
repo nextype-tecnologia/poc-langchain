97ab7dc1514b5b6d87332e603d961f86b97ef306
Natan Carlos Silva <natancarlosss@gmail.com>
2025-06-25 00:07:48 -0300

    Chore: estrutura inicial

---
+++ index.ts
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const { ChatOpenAI } = require('@langchain/openai');
+const { HumanMessage } = require('@langchain/core/messages');
+
+const chat = new ChatOpenAI({
+  temperature: 0.7,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+(async () => {
+  const res = await chat.invoke([
+    new HumanMessage("Qual é a capital da Alemanha?")
+  ]);
+  console.log("Resposta:", res.content);
+})();
