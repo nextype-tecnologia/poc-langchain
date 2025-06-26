79a693180e63210a577177d3f705ee94ae5cbd70
Natan Carlos Silva <natancarlosss@gmail.com>
2025-06-26 19:50:37 -0300

    Chore: enviando corretamente o anexo para o chat GPT

--- index.ts
+++ index.ts
@@ -1,16 +1,43 @@
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 
 const { ChatOpenAI } = require('@langchain/openai');
 const { HumanMessage } = require('@langchain/core/messages');
 
-const chat = new ChatOpenAI({
-  temperature: 0.7,
-  apiKey: process.env.OPENAI_API_KEY,
-});
+try { 
 
-(async () => {
-  const res = await chat.invoke([
-    new HumanMessage("Qual é a capital da Alemanha?")
-  ]);
-  console.log("Resposta:", res.content);
-})();
+    // Caminho para o arquivo .png
+    const imagePath = path.join('./files_examples/energiaeletrica.png');
+    const imageBuffer = fs.readFileSync(imagePath);
+    const base64Image = imageBuffer.toString('base64');
+
+    //console.log(base64Image);
+    //throw new Error();
+
+    const chat = new ChatOpenAI({
+      temperature: 0.1,
+      apiKey: process.env.OPENAI_API_KEY,      
+      modelName: 'gpt-4o', // 👈 Modelo correto com suporte a imagem
+    });
+
+    (async () => {
+      const res = await chat.invoke([
+        new HumanMessage({
+          content: [
+            { type: 'text', text: 'O que há nesta imagem?' },
+            {
+              type: 'image_url',
+              image_url: {
+                url: `data:image/png;base64,${base64Image}`,
+              },
+            },
+          ],
+        })
+      ]);
+      console.log("Resposta:", res.content);
+    })();
+
+} catch (error) {
+    console.error('❌ Erro capturado:', error);
+  }
