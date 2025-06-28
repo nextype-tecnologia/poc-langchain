d01f6ecd195f377a8ecd229d5dd68410e05c47b6
Natan Carlos Silva <natancarlosss@gmail.com>
2025-06-28 15:14:28 -0300

    Chore: integração entre OCR e chat GPT

--- index.ts
+++ index.ts
@@ -2,42 +2,73 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
+const Tesseract = require('tesseract.js');
 const { ChatOpenAI } = require('@langchain/openai');
 const { HumanMessage } = require('@langchain/core/messages');
 
-try { 
+async function processarImagem(linkImagem = '') {
+  try {
+    const { data: { text } } = await Tesseract.recognize(
+      path.join(linkImagem),
+      'por'
+    );
 
-    // Caminho para o arquivo .png
-    const imagePath = path.join('./files_examples/energiaeletrica.png');
-    const imageBuffer = fs.readFileSync(imagePath);
-    const base64Image = imageBuffer.toString('base64');
-
-    //console.log(base64Image);
-    //throw new Error();
-
-    const chat = new ChatOpenAI({
-      temperature: 0.1,
-      apiKey: process.env.OPENAI_API_KEY,      
-      modelName: 'gpt-4o', // 👈 Modelo correto com suporte a imagem
-    });
-
-    (async () => {
-      const res = await chat.invoke([
-        new HumanMessage({
-          content: [
-            { type: 'text', text: 'O que há nesta imagem?' },
-            {
-              type: 'image_url',
-              image_url: {
-                url: `data:image/png;base64,${base64Image}`,
-              },
-            },
-          ],
-        })
-      ]);
-      console.log("Resposta:", res.content);
-    })();
-
-} catch (error) {
-    console.error('❌ Erro capturado:', error);
+    return text;
+  } catch (err) {
+    console.error('Erro no OCR:', err);
   }
+}
+
+async function EnviaLangChain (leituraOCR = ''){
+
+  const chat = new ChatOpenAI({
+    temperature: 0.1,
+    apiKey: process.env.OPENAI_API_KEY,      
+    modelName: 'gpt-4o', // 👈 Modelo correto com suporte a imagem
+  });
+
+  const promptInicial = 'Me ajude a classificar a mensagem a seguir. Preciso que me retorne: data do vencimento, data da competência, valor da conta, nome do fornecedor e determine um plano de contas';
+
+  const res = await chat.invoke([
+
+      new HumanMessage({
+        content: [
+          { 
+            type: 'text', 
+            text: promptInicial
+          },
+          { 
+            type: 'text', 
+            text: leituraOCR
+          },
+          //{
+          //  type: 'image_url',
+          //  image_url: {
+          //    url: `data:image/png;base64,${base64Image}`,
+          //  },
+          //},
+        ],
+      })
+    ]);
+
+    return res;
+    //console.log("Resposta:", res.content);
+}
+
+async function main() {
+  try {
+    const leituraOCR = await processarImagem('./files_examples/energiaeletrica.png');
+
+    //console.log('\nTexto extraído da imagem:\n');
+    //console.log(texto_extraido);
+
+    const res = await EnviaLangChain (leituraOCR);
+
+    console.log("Resposta:", res.content);
+
+  } catch (error) {
+    console.error('❌ Erro capturado na main:', error);
+  }
+}
+
+main();
